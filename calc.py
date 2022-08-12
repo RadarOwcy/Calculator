753f14bbd1b99fb8d206e0952845c126b0cd244a
Maciej Bielinski <maciej.a.bielinski@gmail.com>
2022-08-12 11:32:25 +0200

    Beginning of python calculator

---
+++ calc.py
@@ -0,0 +1,7 @@
+print ("Calculator!")
+first_number=input("Write first number: ")
+first_number_int=int(first_number)
+second_number=input("Write second number: ")
+second_number_int=int(second_number)
+add=first_number_int+second_number_int
+print (first_number+"+"+second_number+"="+str(add))
